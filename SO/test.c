b72692760f5efe8179c6ef3fa6ac07d970e6667a
dumedenisa <150524722+dumedenisa@users.noreply.github.com>
2023-11-17 22:39:20 +0200

    Update test.c

--- SO/test.c
+++ SO/test.c
@@ -1,203 +1,176 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <dirent.h>
-#include <limits.h>
-#include <sys/time.h>
 #include <time.h>
 
-void writeStats(int output_file, const char *filename, int width, int height)
-{
-    struct stat st;
-    if (stat(filename, &st) == -1)
-    {
-        perror("Error at stat");
-        exit(EXIT_FAILURE);
-    }
+#define PATH_MAX_LEN 256
 
-    char modifDate[40];
-    strftime(modifDate, sizeof(modifDate), "%d.%m.%Y", localtime(&st.st_mtime));
+int process_file(const char *file_path, int stat_fd);
 
-    char buffer[256];
-    int dim = snprintf(buffer, sizeof(buffer), "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %ld\n", filename, height, width, st.st_size);
-    if (write(output_file, buffer, dim) == -1)
-    {
-        perror("Error at writing the name on the output file");
-        exit(EXIT_FAILURE);
-    }
-
-    dim = snprintf(buffer, sizeof(buffer), "identificatorul utilizatorului: %d\ntimpul ultimei modificari: %s\ncontorul de legaturi: %ld\n", getuid(), modifDate, st.st_nlink);
-    if (write(output_file, buffer, dim) == -1)
-    {
-        perror("Error at writing to the output file");
-        exit(EXIT_FAILURE);
-    }
-
-    dim = snprintf(buffer, sizeof(buffer), "drepturi de acces user: %c%c%c\ndrepturi de acces grup: %c%c%c\ndrepturi de acces altii: %c%c%c\n",
-                   (st.st_mode & S_IRUSR) ? 'R' : '-',
-                   (st.st_mode & S_IWUSR) ? 'W' : '-',
-                   (st.st_mode & S_IXUSR) ? 'X' : '-',
-                   (st.st_mode & S_IRGRP) ? 'R' : '-',
-                   (st.st_mode & S_IWGRP) ? 'W' : '-',
-                   (st.st_mode & S_IXGRP) ? 'X' : '-',
-                   (st.st_mode & S_IROTH) ? 'R' : '-',
-                   (st.st_mode & S_IWOTH) ? 'W' : '-',
-                   (st.st_mode & S_IXOTH) ? 'X' : '-');
-    if (write(output_file, buffer, dim) == -1)
-    {
-        perror("Error at writing to the output file");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void processEntry(int output_file, const char *entry_name, const char *parent_dir) {
-    if (parent_dir == NULL) {
-        fprintf(stderr, "Parent directory is NULL. Entry name: %s\n", entry_name);
-        exit(EXIT_FAILURE);
-    }
-
-    char full_path[PATH_MAX];
-    if (snprintf(full_path, sizeof(full_path), "%s/%s", parent_dir, entry_name) >= sizeof(full_path)) {
-        fprintf(stderr, "Path too long. Entry name: %s\n", entry_name);
-        exit(EXIT_FAILURE);
-    }
-
-    struct stat st;
-    if (lstat(full_path, &st) == -1) {
-        perror("Error at lstat");
-        fprintf(stderr, "Error processing entry: %s\n", full_path);
-        exit(EXIT_FAILURE);
-    }
-    if (S_ISDIR(st.st_mode))
-    {
-        char buffer[256];
-        int dim = snprintf(buffer, sizeof(buffer), "nume director: %s\nidentificatorul utilizatorului: %d\ndrepturi de acces user: RWX\ndrepturi de acces grup: R--\ndrepturi de acces altii: ---\n", entry_name, getuid());
-        if (write(output_file, buffer, dim) == -1)
-        {
-            perror("Error at writing to the output file");
-            exit(EXIT_FAILURE);
-        }
-
-        DIR *dir = opendir(full_path);
-        if (dir == NULL)
-        {
-            perror("Error at opendir");
-            exit(EXIT_FAILURE);
-        }
-
-        struct dirent *entry;
-        while ((entry = readdir(dir)) != NULL)
-        {
-            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
-            {
-                processEntry(output_file, entry->d_name, full_path);
-            }
-        }
-
-        closedir(dir);
-    }
-    else if (S_ISREG(st.st_mode))
-    {
-        if (strstr(entry_name, ".bmp") != NULL)
-        {
-            int input_file = open(full_path, O_RDONLY);
-            if (input_file == -1)
-            {
-                perror("Error at opening the input file");
-                exit(EXIT_FAILURE);
-            }
-
-            char buffer[256];
-            int dim = snprintf(buffer, sizeof(buffer), "Nume fisier: %s\n", full_path);
-            if (write(output_file, buffer, dim) == -1)
-            {
-                perror("Error at writing the name on the output file");
-                exit(EXIT_FAILURE);
-            }
-
-            int width, height;
-            if (read(input_file, buffer, 18) != -1)
-            {
-                read(input_file, &width, 4);
-                read(input_file, &height, 4);
-            }
-            else
-            {
-                perror("Cannot read the first 18 bytes");
-                exit(EXIT_FAILURE);
-            }
-
-            writeStats(output_file, full_path, width, height);
-
-            close(input_file);
-        }
-        else
-        {
-            char buffer[256];
-            int dim = snprintf(buffer, sizeof(buffer), "Nume fisier: %s\n", full_path);
-            if (write(output_file, buffer, dim) == -1)
-            {
-                perror("Error at writing the name on the output file");
-                exit(EXIT_FAILURE);
-            }
-
-            writeStats(output_file, full_path, -1, -1);
-        }
-    }
-    else if (S_ISLNK(st.st_mode))
-    {
-        char target_path[PATH_MAX];
-        ssize_t target_size = readlink(full_path, target_path, sizeof(target_path) - 1);
-        if (target_size == -1)
-        {
-            perror("Error at readlink");
-            exit(EXIT_FAILURE);
-        }
-
-        target_path[target_size] = '\0';
-
-        char buffer[256];
-        int dim = snprintf(buffer, sizeof(buffer), "nume legatura: %s\ndimensiune legatura: %ld\ndimensiune fisier dimensiunea fisierului target: %ld\n", entry_name, st.st_size, strlen(target_path));
-        if (write(output_file, buffer, dim) == -1)
-        {
-            perror("Error at writing to the output file");
-            exit(EXIT_FAILURE);
-        }
-
-        dim = snprintf(buffer, sizeof(buffer), "drepturi de acces user legatura: RWX\ndrepturi de acces grup legatura: R--\ndrepturi de acces altii legatura: ---\n");
-        if (write(output_file, buffer, dim) == -1)
-        {
-            perror("Error at writing to the output file");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        fprintf(stderr, "Entry is neither a file, directory, nor a symbolic link: %s\n", full_path);
-    }
-}
+void process_directory(const char *dir_path, int stat_fd);
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <director_intrare>\n", argv[0]);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    char *input_directory = argv[1];
-    int output_file = open("statistica.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (output_file == -1) {
-        perror("The output file cannot be opened");
-        exit(EXIT_FAILURE);
+    const char *director_intrare = argv[1];
+
+    int stat_fd = open("statistica.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if (stat_fd == -1) {
+        perror("Eroare la crearea fisierului de statistica");
+        exit(1);
     }
 
-    processEntry(output_file, ".", input_directory);
+    process_directory(director_intrare, stat_fd);
 
-    close(output_file);
+    close(stat_fd);
 
     return 0;
 }
 
+int process_file(const char *file_path, int stat_fd) {
+    int fd = open(file_path, O_RDONLY);
+    if (fd == -1) {
+        perror("Eroare la deschiderea fisierului");
+        return -1;
+    }
+
+    struct stat file_info;
+    if (fstat(fd, &file_info) == -1) {
+        perror("Eroare la obtinerea informatiilor despre fisier");
+        close(fd);
+        return -1;
+    }
+
+    char nume_fisier[256];
+    strcpy(nume_fisier, file_path);
+
+    char timestamp[20];
+    strftime(timestamp, sizeof(timestamp), "%d.%m.%Y", localtime(&file_info.st_mtime));
+
+    mode_t file_mode = file_info.st_mode;
+    char user_permissions[4], group_permissions[4], others_permissions[4];
+
+    user_permissions[0] = (file_mode & S_IRUSR) ? 'R' : '-';
+    user_permissions[1] = (file_mode & S_IWUSR) ? 'W' : '-';
+    user_permissions[2] = (file_mode & S_IXUSR) ? 'X' : '-';
+    user_permissions[3] = '\0';
+
+    group_permissions[0] = (file_mode & S_IRGRP) ? 'R' : '-';
+    group_permissions[1] = (file_mode & S_IWGRP) ? 'W' : '-';
+    group_permissions[2] = (file_mode & S_IXGRP) ? 'X' : '-';
+    group_permissions[3] = '\0';
+
+    others_permissions[0] = (file_mode & S_IROTH) ? 'R' : '-';
+    others_permissions[1] = (file_mode & S_IWOTH) ? 'W' : '-';
+    others_permissions[2] = (file_mode & S_IXOTH) ? 'X' : '-';
+    others_permissions[3] = '\0';
+
+    char statistica[512];
+    if (S_ISLNK(file_info.st_mode)) {
+        char target_path[256];
+        ssize_t target_len = readlink(file_path, target_path, sizeof(target_path) - 1);
+        if (target_len == -1) {
+            perror("Eroare la citirea legaturii simbolice");
+            close(fd);
+            return -1;
+        }
+        target_path[target_len] = '\0';
+
+        sprintf(statistica, "nume legatura: %s\ndimensiune legatura: %lu\ndimensiune fisier target: %lu\ndrepturi de acces user legatura: %s\ndrepturi de acces grup legatura: %s\ndrepturi de acces altii legatura: %s\n\n",
+                nume_fisier, file_info.st_size, file_info.st_size, user_permissions, group_permissions, others_permissions);
+
+        if (write(stat_fd, statistica, strlen(statistica)) == -1) {
+            perror("Eroare la scriere");
+            close(fd);
+            return -1;
+        }
+    } else {
+        sprintf(statistica, "nume fisier: %s\ndimensiune: %lu\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %s\ncontorul de legaturi: %lu\ndrepturi de acces user: %s\ndrepturi de acces grup: %s\ndrepturi de acces altii: %s\n\n",
+                nume_fisier, file_info.st_size, file_info.st_uid, timestamp, file_info.st_nlink, user_permissions, group_permissions, others_permissions);
+
+        if (write(stat_fd, statistica, strlen(statistica)) == -1) {
+            perror("Eroare la scriere");
+            close(fd);
+            return -1;
+        }
+    }
+
+    close(fd);
+    return 0;  // Returnăm 0 pentru a indica succesul operației
+}
+
+
+void process_directory(const char *dir_path, int stat_fd) {
+    DIR *dir = opendir(dir_path);
+    if (!dir) {
+        perror("Eroare la deschiderea directorului");
+        exit(1);
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            char path[PATH_MAX_LEN];
+            int path_len = snprintf(path, PATH_MAX_LEN, "%s/%s", dir_path, entry->d_name);
+            if (path_len >= PATH_MAX_LEN) {
+                fprintf(stderr, "Calea %s/%s este prea lunga.\n", dir_path, entry->d_name);
+                continue;
+            }
+
+            struct stat entry_info;
+            if (lstat(path, &entry_info) == -1) {
+                perror("Eroare la obtinerea informatiilor despre intrare");
+                closedir(dir);
+                exit(1);
+            }
+
+            char statistica[512];
+            if (S_ISDIR(entry_info.st_mode)) {
+                // Procesare director
+                char user_permissions[4], group_permissions[4], others_permissions[4];
+                
+                user_permissions[0] = (entry_info.st_mode & S_IRUSR) ? 'R' : '-';
+                user_permissions[1] = (entry_info.st_mode & S_IWUSR) ? 'W' : '-';
+                user_permissions[2] = (entry_info.st_mode & S_IXUSR) ? 'X' : '-';
+                user_permissions[3] = '\0';
+
+                group_permissions[0] = (entry_info.st_mode & S_IRGRP) ? 'R' : '-';
+                group_permissions[1] = (entry_info.st_mode & S_IWGRP) ? 'W' : '-';
+                group_permissions[2] = (entry_info.st_mode & S_IXGRP) ? 'X' : '-';
+                group_permissions[3] = '\0';
+
+                others_permissions[0] = (entry_info.st_mode & S_IROTH) ? 'R' : '-';
+                others_permissions[1] = (entry_info.st_mode & S_IWOTH) ? 'W' : '-';
+                others_permissions[2] = (entry_info.st_mode & S_IXOTH) ? 'X' : '-';
+                others_permissions[3] = '\0';
+
+                sprintf(statistica, "nume director: %s\nidentificatorul utilizatorului: %d\ndrepturi de acces user: %s\ndrepturi de acces grup: %s\ndrepturi de acces altii: %s\n\n",
+                        entry->d_name, entry_info.st_uid, user_permissions, group_permissions, others_permissions);
+
+                if (write(stat_fd, statistica, strlen(statistica)) == -1) {
+                    perror("Eroare la scriere");
+                    closedir(dir);
+                    exit(1);
+                }
+
+                process_directory(path, stat_fd);
+            } else {
+                // Procesare fisier sau legatura simbolica
+                if (process_file(path, stat_fd) == -1) {
+                    // Tratarea erorilor în funcția process_file
+                    closedir(dir);
+                    exit(1);
+                }
+            }
+        }
+    }
+
+    closedir(dir);
+}
